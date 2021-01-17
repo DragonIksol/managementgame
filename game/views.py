412ecfeddcfc5862eb090ff5f05e94b5a3fd6a5c
BugorSA <vova9840@yandex.ru>
2021-01-17 08:31:52 +0400

    Торги ЕСМ

--- game/views.py
+++ game/views.py
@@ -161,42 +161,7 @@ class FinalTurnView(View):
         })
 
 
-# TODO купить ЕСМ
-class BuyESMView(View):
 
-    def post(self, request, *args, **kwargs):
-        params = json.loads(request.body)
-        esm_count = params.get('esm_count')
-        cost = params.get('cost')
-        game_id = params.get('game_id')
-        error = None
-
-        try:
-            player = PlayerGameInfo.objects.get(player_id=request.user.id, room_id=game_id)
-            esm_request = ESMRequest(esm_count=esm_count, esm_price=cost)
-            esm_request.save()
-            player.esm_request_id = esm_request.id
-            player.player_turn_finish = True
-            player.save()
-
-            all_finish = True
-            all_players = PlayerGameInfo.objects.filter(room_id=game_id)
-            for player in all_players:
-                if not player.player_turn_finish:
-                    all_finish = False
-                    break
-
-            if all_finish:
-                # завершить стадию
-                pass
-        except BaseException as err:
-            print(err)
-            error = str(err)
-
-        return JsonResponse({
-            'success': not error,
-            'error': error
-        })
 
 
 class ProduceEGPView(View):
@@ -235,6 +200,80 @@ class ProduceEGPView(View):
             'error': error
         })
 
+
+# TODO купить ЕСМ
+class BuyESMView(View):
+
+    def post(self, request, *args, **kwargs):
+        params = json.loads(request.body)
+        esm_count = params.get('esm_count')
+        cost = params.get('cost')
+        game_id = params.get('game_id')
+        error = None
+
+        # сохранение заявки
+        player = PlayerGameInfo.objects.get(player_id=request.user.id, room_id=game_id)
+        esm_request = ESMRequest(esm_count=esm_count, esm_price=cost)
+        esm_request.save()
+        player.esm_request_id = esm_request.id
+        player.player_turn_finish = True
+        player.save()
+
+        if check_turn_finish(game_id):
+            self.bank_ESM_bargaining(game_id)
+
+        return JsonResponse({
+            'success': not error,
+            'error': error
+        })
+
+    def bank_ESM_bargaining(self, game_id):
+        game = Game.objects.get(id=game_id)
+        self.esm_count = math.floor(costs_by_level_map[game.level][0] * game.players_count)
+        players = PlayerGameInfo.objects.filter(room_id=game_id)
+        esm_s = []
+        for player in players:
+            esm_s.append(ESMRequest.objects.get(id=player.esm_request_id))
+        self.choose_interested_esm_request(esm_s)
+        return
+
+    def choose_interested_esm_request(self, esm_s):
+        esm_s_price_mass = [x.esm_price for x in esm_s]
+        max_price_esm = max(esm_s_price_mass)
+        esm_request_mass = ESMRequest.objects.filter(esm_price=max_price_esm).exclude(bank_response=True)
+        while (self.esm_count != 0 or esm_request_mass):
+            players = []
+            for esm_request in esm_request_mass:
+                players.append(PlayerGameInfo.objects.filter(
+                    esm_request_id=esm_request.id))
+            self.choose_seniors_esm_request(players)
+        return
+
+    def choose_seniors_esm_request(self, players):
+        players_senior = [x.senioring for x in players]
+        min_senior_rang = min(players_senior)
+        player_with_true_respon = PlayerGameInfo.objects.get(senioring=min_senior_rang)
+        esm_request = ESMRequest.objects.get(id=player_with_true_respon.esm_request_id)
+        self.subtraction_of_bids(esm_request)
+        return
+
+    def subtraction_of_bids(self, esm_request):
+        if self.esm_count >= esm_request.egp_count:
+            self.esm_count = self.esm_count - esm_request.egp_count
+            self.subtraction_capital_of_buy(esm_request.egp_count, esm_request.esm_price, esm_request)
+        else:
+            self.subtraction_capital_of_buy(self.esm_count, esm_request.esm_price, esm_request)
+            self.esm_count = 0
+        return
+
+        # подсчет капитала после продажи
+        def subtraction_capital_of_buy(self, esm_count, esm_price, esm_request):
+            player = PlayerGameInfo.objects.get(egp_request_id=esm_request.id)
+            player.capital = player.capital + esm_count * esm_price
+            player.save()
+            esm_request.bank_response = True
+            esm_request.save()
+            return
 
 # TODO продать ЕГП
 # Продажа ЕГП-Вова
@@ -293,7 +332,9 @@ class SellEGPView(View):
         player_with_true_respon = PlayerGameInfo.objects.get(senioring=min_senior_rang)
         egp_request = EGPRequest.objects.get(id=player_with_true_respon.egp_request_id)
         self.minus_ebuchai_zaiavka(egp_request)
+        return
 
+    #проверка на остаток и закрытие спроса банка
     def minus_ebuchai_zaiavka(self, egp_request):
         if self.egp_count >= egp_request.egp_count:
             self.egp_count = self.egp_count - egp_request.egp_count
@@ -303,6 +344,7 @@ class SellEGPView(View):
             self.egp_count = 0
         return
 
+    #подсчет капитала после продажи
     def add_capital_of_sold(self, egp_count, egp_price, egp_request):
         player = PlayerGameInfo.objects.get(egp_request_id=egp_request.id)
         player.capital = player.capital + egp_count * egp_price
@@ -310,3 +352,48 @@ class SellEGPView(View):
         egp_request.bank_response = True
         egp_request.save()
         return
+
+
+
+
+
+
+
+
+
+# покупка есм
+# class BuyESMView(View):
+#
+#     def post(self, request, *args, **kwargs):
+#         params = json.loads(request.body)
+#         esm_count = params.get('esm_count')
+#         cost = params.get('cost')
+#         game_id = params.get('game_id')
+#         error = None
+#
+#         try:
+#             player = PlayerGameInfo.objects.get(player_id=request.user.id, room_id=game_id)
+#             esm_request = ESMRequest(esm_count=esm_count, esm_price=cost)
+#             esm_request.save()
+#             player.esm_request_id = esm_request.id
+#             player.player_turn_finish = True
+#             player.save()
+#
+#             all_finish = True
+#             all_players = PlayerGameInfo.objects.filter(room_id=game_id)
+#             for player in all_players:
+#                 if not player.player_turn_finish:
+#                     all_finish = False
+#                     break
+#
+#             if all_finish:
+#                 # завершить стадию
+#                 pass
+#         except BaseException as err:
+#             print(err)
+#             error = str(err)
+#
+#         return JsonResponse({
+#             'success': not error,
+#             'error': error
+#         })
